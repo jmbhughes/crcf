9629c65d1a9eadeb5438f56570ab59b6b937c281
jmbhughes <hughes.jmb@gmail.com>
2022-08-24 05:01:04 -0600

    converted CombinationTree to NumpyDocs

--- crcf/tree.py
+++ crcf/tree.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Optional, Type
+from typing import Optional, Type, Tuple
 import numpy as np
 from graphviz import Digraph
 from .rule import Rule, AxisAlignedRule, NonAxisAlignedRule
@@ -22,8 +22,12 @@ class Node:
     bounding_box: Optional[np.ndarray]
 
     def sibling(self) -> Optional[Node]:
-        """
-        :return: the sibling node, returns None if there is no sibling or the parent is not defined
+        """Get sibling node
+
+        Returns
+        -------
+        Node or None
+            the sibling node, returns None if there is no sibling or the parent is not defined
         """
         if self.parent is None:
             return None
@@ -31,8 +35,12 @@ class Node:
             return self.parent.left_child if self.parent.left_child is not self else self.parent.right_child
 
     def depth(self) -> int:
-        """
-        :return: the depth of the node in the tree
+        """Get depth of node
+
+        Returns
+        -------
+        int
+            the depth of the node in the tree
         """
         depth = 0
         current = self
@@ -41,13 +49,13 @@ class Node:
             depth += 1
         return depth
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.is_leaf) + str(self.rule) + str(self.bounding_box)
 
     def __hash__(self):
         return hash(str(self))
 
-    def __eq__(self, other: Node):
+    def __eq__(self, other: Node) -> bool:
         return self.is_leaf == other.is_leaf \
                and self.left_child == other.left_child \
                and self.right_child == other.right_child \
@@ -65,13 +73,16 @@ class CombinationTree:
     def __init__(self, depth_limit: Optional[int] = None,
                  rule_kind: Type[Rule] = AxisAlignedRule,
                  rule_mode: str = "uniform") -> None:
+        """Initialize a combination tree.
 
-        """
-        Initialize a Combination Tree
-        :param depth_limit: the maximum depth the tree can grow to,
-                            a depth limit of None indicates there is no limit
-        :param rule_kind: whether to use axis aligned or non-axis aligned rules
-        :param rule_mode: the mode by which the rules are generated, see the rule for documentation
+        Parameters
+        ----------
+        depth_limit : int or None
+            the maximum depth the tree can grow to, None indicates no depth limiting
+        rule_kind : Rule
+            whether to use axis aligned or non-axis aligned rules
+        rule_mode : str
+            the mode by which the rules are generated, see the rule for documentation
         """
         # properties of the tree
         self.depth_limit = depth_limit
@@ -82,10 +93,20 @@ class CombinationTree:
         self.root = None  # tree has not been grown at all yet
 
     def fit(self, x: np.ndarray) -> None:
-        """
-        Fit a tree from scratch.
-        Note: that if the tree is already fitted this will overwrite completely.
-        :param x: the data points to fit  e.g. x = [[1, 2, 3], [3, 4, 5]] is a list of two 3-d points
+        """Fit a tree from scratch
+
+        Parameters
+        ----------
+        x : np.ndarray
+            the data points to fit  e.g. x = [[1, 2, 3], [3, 4, 5]] is a list of two 3-d points
+
+        Notes
+        -----
+        If the tree is already fitted this will overwrite completely.
+
+        Returns
+        -------
+        None
         """
         self.root = Node(is_leaf=True,
                          parent=None,
@@ -97,12 +118,22 @@ class CombinationTree:
         self._fit(self.root, x)
 
     def _fit(self, node: Node, x: np.ndarray, depth: int = 0) -> None:
+        """Helper function that recursively grows the tree
+
+        Parameters
+        ----------
+        node : Node
+            the current node (the root node of whatever subtree you're growing)
+        x : np.ndarray
+            the points you're fitting
+        depth : int
+            current depth of the node
+
+        Returns
+        -------
+        None
         """
-        Helper function for fit that recursively grows the tree
-        :param node: the current node (the root node of whatever subtree you're growing)
-        :param x: the points you're fitting
-        """
-        # update the rule and local properties with regards to x
+        # update the rule and local properties in regard to x
         node.count = x.shape[0]
         node.bounding_box = np.array([[np.nanmin(x[:, i]), np.nanmax(x[:, i])] for i in range(x.shape[1])])
         # if the depth limit has been hit, the tree can no longer grow
@@ -141,16 +172,25 @@ class CombinationTree:
             self._fit(right_child, x[np.logical_not(evaluation)], depth=depth+1)
 
     def count(self) -> int:
-        """
-        :return: number of data points in the tree
+        """Total count of data points in the tree
+
+        Returns
+        -------
+        number of data points in the tree
         """
         return 0 if self.root is None else self.root.count
 
-    def show(self, path: Optional[str] = None):
-        """
-        draw the tree to a file
-        :param path: the path to save image to
-        :return: a drawn tree
+    def show(self, path: Optional[str] = None) -> None:
+        """Draw the tree
+
+        Parameters
+        ----------
+        path : str or None
+            the path to save the image to
+
+        Returns
+        -------
+        None
         """
         dot = Digraph()
         queue = [(0, self.root)]  # start at the root with no parent
@@ -183,24 +223,38 @@ class CombinationTree:
         # show the graph
         dot.view(filename=path)
 
-    def find(self, x: np.ndarray) -> (Node, int):
+    def find(self, x: np.ndarray) -> Tuple[Node, int]:
+        """Find the correct leaf node for an input point x
+
+        Parameters
+        ----------
+        x : np.ndarray
+             a single data point
+
+        Returns
+        -------
+        Tuple[Node, int]
+            the leaf node that x would belong to, and the depth of that node
         """
-        Find the correct leaf node for an input point x
-        :param x: a data point
-        :return: the leaf node x would belong with, depth of that node
-        """
-        node = self.root
-        depth = 0
+        node: Node = self.root
+        depth: int = 0
         while not node.is_leaf:
             node = node.left_child if node.rule.evaluate(np.array([x])) else node.right_child
             depth += 1
         return node, depth
 
     def get_node(self, label: str) -> Node:
-        """
-        Find a node using its label
-        :param label: label of node
-        :return: the node with that label
+        """Find a node using its label
+
+        Parameters
+        ----------
+        label : str
+            label of a node
+
+        Returns
+        -------
+        Node
+            the node with the given label
         """
         try:
             node = self.labels[label]
@@ -210,9 +264,16 @@ class CombinationTree:
             return node
 
     def save(self, path: str) -> None:
-        """
-        Save the tree to a file for later opening
-        :param path: the path/name of the new file, should have '.pkl' extension
+        """Save the tree to a file for later opening
+
+        Parameters
+        ----------
+        path : str
+            the path/name of the new file, should have '.pkl' extension
+
+        Returns
+        -------
+        None
         """
         # TODO: change this to a more robust save method
         if not path.lower().endswith('.pkl'):
@@ -222,10 +283,17 @@ class CombinationTree:
 
     @classmethod
     def load(cls, path: str) -> CombinationTree:
-        """
-        Load a tree from a file
-        :param path: location of the file
-        :return: the loaded tree
+        """Load a tree from a file
+
+        Parameters
+        ----------
+        path : str
+            the location of the file
+
+        Returns
+        -------
+        CombinationTree
+            the loaded tree
         """
         with open(path, 'rb') as f:
             tree = pickle.load(f)
@@ -233,24 +301,48 @@ class CombinationTree:
 
     def depth(self, x: np.ndarray,
               estimated: bool = True) -> float:
-        """
-        Determine the depth of where x is in the tree
-        :param x: a point
-        :param estimated: if True will use the counts at a leaf node
-            to estimate how far down the tree the point would be if it had been grown completely
-        :return: the depth of the point
+        """Determine the depth of x in the tree
+
+        Parameters
+        ----------
+        x : np.ndarray
+            a data point
+        estimated : bool
+            if True will use the counts at a leaf node to estimate how far down the tree the point
+                would be if it had been grown completely
+
+        Returns
+        -------
+        int
+            the depth of the point
         """
         def harmonic(n: int) -> float:
-            """
-            :param n: index
-            :return: the nth harmonic number
+            """n-th harmonic number
+
+            Parameters
+            ----------
+            n : int
+                index of the harmonic number
+
+            Returns
+            -------
+            float
+                the n-th harmonic number
             """
             return np.log(n) + np.euler_gamma
 
         def expected_length(n: int) -> float:
-            """
-            :param n: count remaining in leaf node
-            :return: the expected average length had the tree continued to grow
+            """Expected depth in the tree given count in the leaf node
+
+            Parameters
+            ----------
+            n : int
+                the number of data points in a leaf node
+
+            Returns
+            -------
+            float
+                the expected average length had the tree continued to grow
             """
             return 2 * harmonic(n-1) - (2*(n-1) / n) if n > 1 else 0
 
@@ -259,20 +351,39 @@ class CombinationTree:
         return depth + extension
 
     def displacement(self, x: np.ndarray) -> float:
-        """
-        The displacement of a point x in the tree
-        :param x: a data sample
-        :return: the "surprise" or displacement induced by including x in the tree
+        """Displacement of a point x in the tree
+
+        Parameters
+        ----------
+        x : np.ndarray
+            a single data point
+
+        Returns
+        -------
+        float
+            the "surprise" or displacement induced by including x in the tree
         """
         leaf, depth = self.find(x)
         sibling = leaf.parent.left_child if leaf.parent.left_child is not leaf else leaf.parent.right_child
         return sibling.count
 
     def codisplacement(self, x: np.ndarray) -> float:
-        """
+        """Get codisplacement of a data point
+
+        Parameters
+        ----------
+        x : np.ndarray
+            a data sample
+
+        Notes
+        -----
         Codisplacement allows for colluders in the displacement per RRCF paper [Guha+2016]
-        :param x: a data sample
-        :return: the collusive displacement induced by including x in the tree
+
+        Returns
+        -------
+        float
+            codisplacement of a data point
+
         """
         node, depth = self.find(x)
         best_codisp = 0
@@ -327,8 +438,15 @@ class CombinationTree:
         return depth_weight * depths + disp_weight * disp
 
     def remove_by_node(self, node: Node) -> None:
-        """
-        Remove the subtree at this node using the RRCF rules.
+        """Remove the specified node using RRCF rules
+
+        Parameters
+        ----------
+        node : Node
+            node to remove
+
+        Notes
+        -----
         below we delete the R node:
                    |               |
                    G               G
@@ -336,7 +454,11 @@ class CombinationTree:
                  o   P    TO     o  S
                     / |
                    S   R
-        :param node: node at root of tree or single node to remove
+
+        Returns
+        -------
+        None
+
         """
         node_to_delete = node
         parent = node_to_delete.parent
@@ -353,9 +475,16 @@ class CombinationTree:
             self._remove_normal(node)
 
     def _remove_normal(self, node: Node) -> None:
-        """
-        Remove a node that is deeper in the tree, i.e. it is not a child of the root
-        :param node: the root of the subtree to delete
+        """Remove a node that is deeper in the tree, i.e. it is not a child of the root
+
+        Parameters
+        ----------
+        node : Node
+            node that is not the root of a tree
+
+        Returns
+        -------
+        None
         """
         self._clear_labels(node)
 
@@ -367,9 +496,16 @@ class CombinationTree:
         # TODO: update the bounding boxes
 
     def _remove_gen1(self, node: Node) -> None:
-        """
-        Remove a node that is a child of the root, meaning that the sibling becomes the new root
-        :param node: root of the subtree to remove
+        """Remove a node that is a child of the root, meaning that the sibling becomes the new root
+
+        Parameters
+        ----------
+        node : Node
+            the node to remove
+
+        Returns
+        -------
+        None
         """
         self._clear_labels(node)
         sibling = node.sibling()
@@ -377,15 +513,23 @@ class CombinationTree:
         self.root.parent = None
 
     def _clear_labels(self, node: Node) -> None:
-        """
-        Clear the labels of all nodes in the subtree rooted at node
-        :param node: the root of the subtree to clear
+        """Clear the labels of all nodes in the subtree rooted at node
+
+
+        Parameters
+        ----------
+        node : Node
+            the root of the subtree to clear
+
+        Returns
+        -------
+        None
         """
         nodes = [node]
         while nodes:
             current = nodes.pop()
 
-            # if its not a leaf it has children so add them to clear next
+            # if it's not a leaf it has children so add them to clear next
             if not current.is_leaf:
                 nodes.append(current.left_child)
                 nodes.append(current.right_child)
@@ -397,18 +541,36 @@ class CombinationTree:
                 del self.labels[current]
 
     def remove_by_label(self, label: str) -> None:
-        """
-        Remove a node from its label
-        Raises a KeyError if a node does not exist with that label
-        :param label: the label of the node
+        """Remove a node by its label
+
+        Parameters
+        ----------
+        label : str
+            label of a node
+
+        Raises
+        ------
+        KeyError
+            if a node does not exist with that label
+
+        Returns
+        -------
+        None
         """
         node = self.get_node(label)
         self.remove_by_node(node)
 
     def remove_by_value(self, x: np.ndarray) -> None:
-        """
-        Removes a node based associated with the value x
-        :param x: the value of node
+        """Removes a node associated with the value x
+
+        Parameters
+        ----------
+        x : np.ndarray
+            a data point
+
+        Returns
+        -------
+        None
         """
         node, depth = self.find(x)
         self.remove_by_node(node)
@@ -421,11 +583,24 @@ class CombinationTree:
     #     """
     #     pass
 
-    def name(self, node: Node, label: str):
-        """
-        Assign the specified node the passed label. If the label is already used raises a KeyError.
-        :param node: node to name
-        :param label: label to give node
+    def name(self, node: Node, label: str) -> None:
+        """Assign the specified node the passed label.
+
+        Parameters
+        ----------
+        node : Node
+            which node to name
+        label : str
+            label to give the node
+
+        Raises
+        ------
+        KeyErorr
+            If the label is already used, it raises a KeyError.
+
+        Returns
+        -------
+        None
         """
         if label in self.labels:
             raise KeyError("A node already exists labeled {}".format(label))
